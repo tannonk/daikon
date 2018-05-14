61555eeeb087df242643ca8db33fbf6845191a9a
Tannon Kew <tannon@tannons-macbook-pro.home>
2018-05-14 18:44:59 +0200

    bidirectional encoder RNN

--- daikon/compgraph.py
+++ daikon/compgraph.py
@@ -51,17 +51,29 @@ def define_computation_graph(source_vocab_size: int, target_vocab_size: int, bat
         #                                                          dtype=tf.float32)
 
         ## Bidirectional RNN ##
-        ((encoder_fw_outputs, encoder_bw_outputs),
-         (encoder_fw_final_state, encoder_bw_final_sate)) = (tf.nn.bidirectional_dynamic_rnn(cell_fw=encoder_cell, cell_bw=encoder_cell, inputs=encoder_inputs_embedded, dtype=tf.float32, time_major=True))
+        ((encoder_fw_outputs, encoder_bw_outputs), (encoder_fw_final_state, encoder_bw_final_sate)) = (tf.nn.bidirectional_dynamic_rnn(cell_fw=encoder_cell,
+        cell_bw=encoder_cell, inputs=encoder_inputs_embedded, dtype=tf.float32, time_major=True))
+
+        encoder_outputs = tf.concat((encoder_fw_outputs,
+                                     encoder_bw_outputs), 2)
 
 
-        encoder_outputs_bi = tf.concat((encoder_fw_outputs,
-                                        encoder_bw_outputs),
-                                       1)
+        # encoder_outputs_bi = tf.concat((encoder_fw_outputs,
+        #                                 encoder_bw_outputs),
+        #                                1)
 
-        encoder_final_state_bi = tf.concat((encoder_fw_final_state,
-                                            encoder_bw_final_sate),
-                                           1)
+        encoder_final_state_c = tf.concat((encoder_fw_final_state.c,
+                                           encoder_bw_final_state.c), 1)
+
+        encoder_final_state_h = tf.concat((encoder_fw_final_state.h,
+                                           encoder_bw_final_state.h), 1)
+
+        encoder_final_state_bi = LSTMStateTuple(c=encoder_final_state_c,
+                                             h=encoder_final_state_h)
+
+        # encoder_final_state_bi = tf.concat((encoder_fw_final_state,
+        #                                     encoder_bw_final_sate),
+        #                                    1)
 
 
     # Original decoder initialisation.
